714a08b0f4041334db6d4c7fb9de7f8d5c3acc07
Dylan Palmboom <nofxboy1234@gmail.com>
2023-05-29 12:19:48 +0200

    Implement Game#load_dictionary

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
+require 'yaml'
+
 # The Game class represents a game in Hangman
 class Game
+  attr_reader :dictionary
+
   def initialize
 
   end
 
   def load_dictionary(text_file)
-    
+    @dictionary = File.readlines(text_file, chomp: true)
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe Game do
   describe '#load_dictionary' do
     context 'when the dictionary is loaded successfully from the text file' do
       it 'returns true' do
-        text_file = 'google-10000-english-no-swears'
-        expect(game.load_dictionary(text_file)).to eq(true)
+        text_file = 'google-10000-english-no-swears.txt'
+        expect(File).to receive(:readlines)
+
+        game.load_dictionary(text_file)
       end
     end
   end
