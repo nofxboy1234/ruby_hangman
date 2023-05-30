85ce94408265c44ff44ff0d4fd2c1279f4a86bf3
Dylan Palmboom <nofxboy1234@gmail.com>
2023-05-30 14:19:57 +0200

    Update main.rb script file

--- lib/main.rb
+++ lib/main.rb
@@ -2,6 +2,16 @@
 
 require_relative 'game'
 
+def player_turn(game)
+  puts 'Enter your guess'
+  guess = gets.strip.chomp.downcase
+  indices = game.indices_of_letter(guess)
+
+  game.update_guess_word(guess, indices) if game.correct_letter?(guess)
+
+  p game.guess_word
+end
+
 game = Game.new
 game.load_dictionary
 game.select_word
@@ -9,10 +19,4 @@ game.select_word
 p game.secret_word
 p Array.new(game.secret_word.length, '_')
 
-puts 'Enter your guess'
-guess = gets.strip.chomp.downcase
-indices = game.indices_of_letter(guess)
-
-game.update_guess_word(guess, indices) if game.correct_letter?(guess)
-
-p game.guess_word
+player_turn(game)
