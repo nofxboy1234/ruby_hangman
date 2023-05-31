89ecc782d3cbf22a01ebcab2ad053b41525c04f6
Dylan Palmboom <nofxboy1234@gmail.com>
2023-05-31 12:35:37 +0200

    Update main script file to use a loop instead of recursion for game loop

--- lib/main.rb
+++ lib/main.rb
@@ -4,13 +4,20 @@ require_relative 'game'
 
 require 'pry-byebug'
 
-def player_turn(game)
+def display_info(game)
   system 'clear'
   p game.secret_word
-
   p game.guess_word
+  puts "incorrect guesses: #{game.incorrect_guesses}"  
+end
 
-  puts "incorrect guesses: #{game.incorrect_guesses}"
+def player_turn(game)
+  display_info(game)
+  # system 'clear'
+  # p game.secret_word
+  # p game.guess_word
+  # puts "incorrect guesses: #{game.incorrect_guesses}"
+
   puts "You have #{game.guesses} incorrect guesses left"
   puts 'Enter your guess'
   guess = gets.strip.chomp.downcase
@@ -25,29 +32,29 @@ def player_turn(game)
   game.update_guess_word
 end
 
-def new_game
-  binding.pry
-  game = Game.new
-  game.load_dictionary
-  game.select_word
-  game.update_guess_word
-  
-  player_turn(game) until game.over?
-  
-  if game.secret_word_guessed?
-    puts 'You guessed the secret word!'
-  else
-    puts 'Game over. You ran out of incorrect guesses!'
-  end
-  
-  puts 'Play again? (y = yes / any other character = no)'
-  answer = gets.strip.chomp.downcase
-  
-  if answer == 'y'
-    new_game
+def game_loop
+  loop do
+    # binding.pry
+    game = Game.new
+    game.load_dictionary
+    game.select_word
+    game.update_guess_word
+    
+    player_turn(game) until game.over?
+    display_info(game)
+
+    if game.secret_word_guessed?
+      puts 'You guessed the secret word!'
+    else
+      puts 'Game over. You ran out of incorrect guesses!'
+    end
+    
+    puts 'Play again? (y = yes / any other character = no)'
+    play_again = gets.strip.chomp.downcase
+    break unless play_again == 'y'
   end
 end
 
-new_game
-binding.pry
+game_loop
+# binding.pry
 puts 'end'
