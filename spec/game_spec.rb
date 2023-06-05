1cd310ec1c65fccf28d5e2ddd9eb8ec0100b61c0
Dylan Palmboom <nofxboy1234@gmail.com>
2023-06-05 11:38:23 +0200

    Add tests for Game#update_guess_word

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -78,9 +78,34 @@ RSpec.describe Game do
 
     context 'when the guessed letter is a letter not inside the secret word' do
       let(:letter) { 'z' }
-      
+
       it 'returns false' do
         expect(game_normal.correct_letter?(letter)).to eq(false)
+      end
+    end
+  end
+
+  describe '#update_guess_word' do
+    
+    context 'when the guessed letter is a letter inside the secret word' do
+      let(:letter) { 'n' }
+
+      it 'updates the guess_word array at the indices where the guessed letter is found' do
+        allow(game_normal).to receive(:secret_word).and_return(secret)
+        game_normal.update_guess_word(letter)
+
+        expect(game_normal.instance_variable_get(:@guess_word)).to eq(%w[_ n _ _ _])
+      end
+    end
+
+    context 'when the guessed letter appears multiple times in the secret word' do
+      let(:letter) { 'e' }
+
+      it 'updates the guess_word array at the indices where the guessed letter is found' do
+        allow(game_normal).to receive(:secret_word).and_return(secret)
+        game_normal.update_guess_word(letter)
+
+        expect(game_normal.instance_variable_get(:@guess_word)).to eq(%w[_ _ e e _])
       end
     end
   end
